785bb2985b88e02ff64b622fff9a210b278e8c43
hymd <hymd.email@gmail.com>
2022-07-23 17:36:20 +0900

    fix: cmp priorities.

--- cmp.lua
+++ cmp.lua
@@ -4,8 +4,8 @@ return {
   source_priority = {
     nvim_lsp = 1000,
     luasnip = 800,
-    treesitter = 600,
-    buffer = 400,
-    path = 200,
+    buffer = 600,
+    path = 400,
+    treesitter = 200,
   },
 }
